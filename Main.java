5cf77ea2a69641101418ef28c1052cf3024c3aaf
Pahanpretty <115658991+Pahanpretty@users.noreply.github.com>
2022-10-28 20:01:31 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+
+
+
+        int[] numbers = new int[5];
+
+        numbers[0] = 10;
+        System.out.println(numbers[0]);
+    }
+}
